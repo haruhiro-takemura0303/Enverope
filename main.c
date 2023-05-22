d1385bfe19522348b8c1267760624acfcb71f284
Haruhiro <takenokonomura0303@gmail.com>
2023-05-22 13:25:52 +0000

    fix

--- main.c
+++ main.c
@@ -1,32 +1,46 @@
 # include <stdio.h>
 # include <stdint.h>
 
+int32_t gPot1;
+int32_t invTa;
+
 int32_t expArg;
 int32_t expValue;
 
-int32_t calcExponential(int32_t x) //Q.8
+int32_t calcExponential(int32_t x) //Q.15
 {
     int32_t expAns;
-    int32_t doublePoly;
+    int32_t Poly;
 
     if (x < 0){
-        x = -1*x;
-        doublePoly = x*x>>9;
-        expAns = 1*(1<<8) - x + doublePoly;
+        x = x*(-1);
+        printf("%d\n",x);
+        Poly = ((x*x)>>1) - (x<<15);
+        expAns = 1*(1<<15) + (Poly>>15);
+        printf("poly : %x\n",Poly);
     }
     else{
-        doublePoly = x*x>>9;
-        expAns = 1*(1<<8) + x + doublePoly; 
+        Poly = (x<<15) + ((x*x)>>1);
+        expAns = 1*(1<<15) +(Poly>>15);
+        printf("poly : %x\n",Poly);
+        printf("expAns : %x\n",expAns);
     }
 
-    return expAns; //Q.8
+    return expAns; //Q.15
 }
 
 int main(void)
 {
-expArg = 0.9*(1<<8);
-expValue = calcExponential(expArg);
-int16_t ans = expValue>>8;
-printf("%d\n",ans);
+//define param
+gPot1 = 1000;
+invTa = (-1)*(2*(1<<15) / gPot1);
+
+int16_t checkTa = invTa; 
+printf("invTa %x\n",checkTa);
+
+//calc exponent
+expValue = calcExponential(invTa);
+int16_t ans = expValue>>15;
+printf("Ans %d\n",ans);
 
 }
